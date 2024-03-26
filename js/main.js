ebc9ca2cc0125819cac231d19610065b0a7b5ee8
Aymansedkey10 <am3423831@gmail.com>
2024-03-26 14:45:01 +0200

    counter

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let valueOfNumber = document.querySelector(".container-value");
+let increment = document.querySelector(".increment");
+let decrement = document.querySelector(".decrement");
+let counter = 0 ;
+
+valueOfNumber.textContent = 0 ;
+decrement.addEventListener('click', ()=>{
+    if (counter == 20) {
+        alert("The maximum Number = 20");
+    } else {
+        counter++;
+        valueOfNumber.textContent = counter ;
+        valueOfNumber.style.color = "#0d6efd";
+    }
+   
+});
+increment.addEventListener('click', ()=>{
+    if (counter == 0) {
+        alert("This is Minimum Number");
+    }else{
+        counter--;
+        valueOfNumber.textContent = counter ;
+        valueOfNumber.style.color = "red";
+    }
+});
